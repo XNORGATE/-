b1119bbad8985841cf1cac290647954d120b3629
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2021-01-05 13:29:20 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html><body>
+  <style>
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: black;
+  text-align: center;
+}
+h1 {
+  line-height: 18.0;
+}
+h1 {
+  text-shadow: 2px 2px 5px red;
+}
+</style>
+
+<h1>
+  <center>
+  <font size="200"width="100" height="100">網頁閒置過久=> 
+  <a href="/index.php">🔄🏡🔄
+  </a><=按此返回登入畫面
+  </font>
+  <center>
+</h1>
+
+</body>
+</html>
