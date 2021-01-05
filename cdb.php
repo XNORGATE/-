8c8edfc26d5980a778237be25dfd2dfe44b57d12
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2021-01-05 13:25:53 +0800

    Add files via upload

---
+++ cdb.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+// REPLACE with your Database name
+$dbname = "zmxnqroi_daandhtproject";
+// REPLACE with Database user
+$username = "zmxnqroi";
+// REPLACE with Database user password
+$password = "5V43zDbes9";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
